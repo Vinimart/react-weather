7436ea3b99eb5e7a4f6977e7e8c06f225fc81a4e
Vinicius Martins <vinimartdev@gmail.com>
2020-11-12 00:00:23 -0300

    Basic structure of the main components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import apiKey from "./apiKey"
+import apiKey from "./apiKey";
 
 function App() {
-  return (
-    <div className="App">
-      <div>Hello!</div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<main>
+				<div className="weather-container">
+					<div className="search-box">
+						<input type="text" className="search-bar" placeholder="Cidade"></input>
+					</div>
+
+					<div className="weather-main">
+						<div className="location-box">
+							<div className="location">Brasília, BR</div>
+							<div className="date">Quarta-feira, 11 de nov. de 2020</div>
+						</div>
+
+						<div class="weather-box">
+							<div class="temp">21ºC</div>
+							<div class="weather">Nublado</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
