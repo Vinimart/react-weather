da0c02225af1f02aaab1c51b705eef6862cafabf
Vinicius Martins <vinimartdev@gmail.com>
2020-11-12 21:18:16 -0300

    API get via SSL

--- src/components/ApiKey.js
+++ src/components/ApiKey.js
@@ -1,6 +1,6 @@
 const apiKey = {
 	key: "2f66048c0472a33ffb7149e006c911b5",
-	base: "http://api.openweathermap.org/data/2.5/",
+	base: "https://api.openweathermap.org/data/2.5/",
 };
 
 export default apiKey;
